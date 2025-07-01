6228912efc26b5f22c7fe98b437cf03d29985492
JL <joaolori@gmail.com>
2025-07-01 16:34:15 +0100

    trt

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from .model_pgn import PGNModel
 from .utils import decode_labels, inv_preprocess, prepare_label, save, load
 from .ops import conv2d, max_pool, linear
 from .image_reader import create_dataset
-from .image_reader_pgn import ImageReaderPGN
+# from .image_reader_pgn import ImageReaderPGN
