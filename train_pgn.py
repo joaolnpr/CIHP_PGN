3d5e356f910f83fe36051f4a0c43ec4d6f5249f0
JL <joaolori@gmail.com>
2025-07-01 16:30:12 +0100

    ghgf

--- train_pgn.py
+++ train_pgn.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 import numpy as np
 import random
 from utils.utils import prepare_label
-from utils.model_pgn import PGNModel
+from utils.pgn_keras import PGNKeras
+from utils.image_reader import create_dataset
 
 # Set gpus
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
@@ -33,66 +34,51 @@ with open(DATA_ID_LIST, 'r') as f:
 SAVE_PRED_EVERY = TRAIN_SET // BATCH_SIZE + 1
 NUM_STEPS = SAVE_PRED_EVERY * 100 + 1
 
-IMG_MEAN = np.array((125.0, 114.4, 107.9), dtype=np.float32)
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
 
-def preprocess_image(img_path):
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.cast(img, tf.float32)
-    img_r, img_g, img_b = tf.split(img, 3, axis=2)
-    img = tf.concat([img_b, img_g, img_r], 2)
-    img -= IMG_MEAN
-    img = tf.image.resize(img, INPUT_SIZE)
-    return img
+# Prepare dataset
+train_dataset = create_dataset(
+    data_dir=DATA_DIR,
+    data_list=LIST_PATH,
+    data_id_list=DATA_ID_LIST,
+    input_size=INPUT_SIZE,
+    random_scale=RANDOM_SCALE,
+    random_mirror=RANDOM_MIRROR,
+    batch_size=BATCH_SIZE,
+    shuffle=SHUFFLE
+)
 
-def main():
-    random.seed(42)
-    np.random.seed(42)
-    tf.random.set_seed(42)
+# Model
+model = PGNKeras(n_classes=N_CLASSES)
+optimizer = tf.keras.optimizers.SGD(learning_rate=LEARNING_RATE, momentum=MOMENTUM)
 
-    # Load and preprocess images
-    image_list = [line.strip() for line in open(LIST_PATH)]
-    image_paths = [os.path.join(DATA_DIR, p.split()[0]) for p in image_list]
-    label_paths = [os.path.join(DATA_DIR, p.split()[1]) for p in image_list]
-    images = [preprocess_image(p) for p in image_paths]
-    labels = [tf.io.read_file(p) for p in label_paths]
-    labels = [tf.image.decode_png(l, channels=1) for l in labels]
-    labels = [tf.image.resize(l, INPUT_SIZE, method='nearest') for l in labels]
-    images = tf.stack(images)
-    labels = tf.stack(labels)
+@tf.function
+def train_step(image_batch, label_batch, edge_batch):
+    with tf.GradientTape() as tape:
+        parsing_fc, parsing_rf_fc, edge_rf_fc = model(image_batch, training=True)
+        label_proc = prepare_label(label_batch, tf.shape(parsing_fc)[1:3], one_hot=False)
+        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=parsing_fc, labels=label_proc))
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    return loss
 
-    # Model (must refactor PGNModel to be a tf.keras.Model subclass for best practice)
-    model = PGNModel({'data': images}, is_training=True, n_classes=N_CLASSES, keep_prob=0.9)
-    parsing_out1 = model.layers['parsing_fc']
-    parsing_out2 = model.layers['parsing_rf_fc']
-    edge_out = model.layers['edge_rf_fc']
-
-    # Loss (example, you may need to adapt for your model)
-    label_proc = prepare_label(labels, tf.shape(parsing_out1)[1:3], one_hot=False)
-    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=parsing_out1, labels=label_proc))
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate=LEARNING_RATE, momentum=MOMENTUM)
-
-    @tf.function
-    def train_step():
-        with tf.GradientTape() as tape:
-            logits = model({'data': images}, training=True)
-            loss_value = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=label_proc))
-        grads = tape.gradient(loss_value, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
-        return loss_value
-
-    # Training loop
-    for step in range(NUM_STEPS):
+# Training loop
+step = 0
+for epoch in range(NUM_STEPS // SAVE_PRED_EVERY + 1):
+    for image_batch, label_batch, edge_batch in train_dataset:
         start_time = time.time()
-        loss_value = train_step().numpy()
+        loss_value = train_step(image_batch, label_batch, edge_batch).numpy()
         duration = time.time() - start_time
         if step % SAVE_PRED_EVERY == 0:
             model.save_weights(os.path.join(SNAPSHOT_DIR, f'ckpt_{step}'))
         print(f'step {step} \t loss = {loss_value:.3f} ({duration:.3f} sec/step)')
-
-if __name__ == '__main__':
-    main()
+        step += 1
+        if step >= NUM_STEPS:
+            break
+    if step >= NUM_STEPS:
+        break
 
 
 ##########################################
